70ccfc0ac22e330076d4e431d3c6372da113a40b
raudelgar <raudel24@gmail.com>
2020-08-22 09:18:58 -0400

    nav completed

---
+++ src/muiTheme.js
@@ -0,0 +1,12 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+export default createMuiTheme({
+	palette: {
+		primary: {
+			main: '#91e4c9',
+		},
+	},
+	typography: {
+		fontFamily: `"Roboto", "Helvetica", "Arial", sans-serif`,
+	},
+});
