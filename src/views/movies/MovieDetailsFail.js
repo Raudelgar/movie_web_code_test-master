390f968d400adae26eff1bd83ce897a08ce1adcd
raudelgar <raudel24@gmail.com>
2020-08-22 23:39:35 -0400

    error handler completed

--- src/views/movies/MovieDetailsFail.js
+++ src/views/movies/MovieDetailsFail.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
 import './movieDetails.scss';
+import { ReactComponent as ExclamationSVG } from '../../static/exclamation.svg';
 
 export default function MoviesDetailsFail() {
 	return (
-		<div className='movie'>
-			<h3>...Oops! No Description available for this movie, try again</h3>
+		<div className='movie-detials-container-error'>
+			<h3 className='movie-detials-error-msg'>Oops, something went wrong!</h3>
+			<div className='movie-detials-error-svg'>
+				<ExclamationSVG />
+			</div>
 		</div>
 	);
 }
